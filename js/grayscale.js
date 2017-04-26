59b5d7c4bc182785015d8eb630e71eda618ef013
Richard Pablo <richardpablop@gmail.com>
2017-04-26 17:45:11 +0100

    Updating map pointer

--- js/grayscale.js
+++ js/grayscale.js
@@ -65,7 +65,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(53.3424,-6.2406), // New York
+        center: new google.maps.LatLng(53.343077, -6.243911), // Dublin
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -193,7 +193,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(53.342423,-6.2406834);
+    var myLatLng = new google.maps.LatLng(53.343077, -6.243911);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
